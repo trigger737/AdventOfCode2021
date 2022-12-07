d7a5e3b9912ef44782226252fbcc87fcf5d01b80
trigger737 <Papirnicka14>
2022-12-07 08:57:38 +0100

    2

--- Day 3/Part2.py
+++ Day 3/Part2.py
@@ -1,48 +1,25 @@
-import re
-with open('input1.txt', 'r') as file:
-    data = file.read().strip().split('\n')
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+    diagnostics = [entry.strip() for entry in lines]
 
-print(data)
-# gamma = b''
-# epsilon = b''
+oxygen_diagnostics = diagnostics
 
-o2_nums = []
-co2_nums = []
-columns = []
-for i in range(len(data[0])):
-    column = []
-    for x in data:
-        column.append(x[i])
+for i in range(len(diagnostics[0])):
+    if len(oxygen_diagnostics) == 1:
+        break
+    all_entries_at_pos = [entry[i] for entry in oxygen_diagnostics]
+    common_bit = '1' if all_entries_at_pos.count('1') >= len(oxygen_diagnostics) / 2 else '0'
+    oxygen_diagnostics = [entry for entry in oxygen_diagnostics if entry[i] == common_bit]
+oxygen_rating = int(oxygen_diagnostics[0], base=2)
+print('oxygen', oxygen_diagnostics[0], oxygen_rating)
 
-    if column.count('0') <= column.count('1'):
-        o2_nums.append('1')
-        co2_nums.append('0')
-    else:
-        o2_nums.append('0')
-        co2_nums.append('1')
-    columns.append(''.join(column))
-o2_nums = ''.join(o2_nums)
-co2_nums = ''.join(co2_nums)
-print(o2_nums,co2_nums)
-
-
-print()
-
-
-# for i,k in enumerate(o2_nums):
-#     for j,l in enumerate(data):
-#         # print(i,k,j,l)
-#
-#         if l[i] != k:
-#             # data.pop(j)
-#             data.remove(l)
-#             print(data)
-
-
-
-
-
-
-
-
-
+co2_diagnostics = diagnostics
+for i in range(len(diagnostics[0])):
+    if len(co2_diagnostics) == 1:
+        break
+    all_entries_at_pos = [entry[i] for entry in co2_diagnostics]
+    least_common_bit = '0' if all_entries_at_pos.count('1') >= len(co2_diagnostics) / 2 else '1'
+    co2_diagnostics = [entry for entry in co2_diagnostics if entry[i] == least_common_bit]
+co2_rating = int(co2_diagnostics[0], base=2)
+print('co2', co2_diagnostics[0], co2_rating)
+print('life support rating', oxygen_rating * co2_rating)
